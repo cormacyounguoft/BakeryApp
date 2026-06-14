0daf0639024578e2b8781cf433a3a40c0abac479
cormacyounguoft <cormac.young@mail.utoronto.ca>
2026-06-13 21:02:48 -0400

    Initial Commit

---
+++ BakeryApp/BakeryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BakeryAppApp.swift
+//  BakeryApp
+//
+//  Created by Cormac Young on 2026-06-13.
+//
+
+import SwiftUI
+
+@main
+struct BakeryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BakeryApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BakeryApp
+//
+//  Created by Cormac Young on 2026-06-13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
